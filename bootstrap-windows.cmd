852d64012b7169330d6ae27faafb0088f07715a0
Brian Spratke <brian.spratke@dematic.com>
2021-11-12 09:54:37 -0500

    quick updates.

--- bootstrap-windows.cmd
+++ bootstrap-windows.cmd
@@ -1,9 +1,13 @@
-@echo Requires git installed and an account on github
-@set STARTTIME=%time% 
+@ECHO Requires git installed and an account on github
+@SET STARTTIME=%time% 
 
-call "C:\Progra~2\Micros~3\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+git submodule update --init --recursive
 
-@echo cd to vcpkg
+if "%VSINSTALLDIR%"=="" call "C:\Progra~2\Micros~3\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+
+if exist "%VCPKG_HOME%\bootstrap-vcpkg.bat" goto VCPKG_COMPLETE
+
+@ECHO cd to vcpkg
 cd vcpkg
 call bootstrap-vcpkg.bat
 
@@ -12,17 +16,24 @@ call bootstrap-vcpkg.bat
 .\vcpkg install curl:x64-windows
 .\vcpkg install jansson:x64-windows
 
-REM Please Note:  The paths to the vcpkg builds are set in the VS project files.  I couldn't get the 32bit and 64 bit to work correctly.  
+@REM Please Note:  The paths to the vcpkg builds are set in the VS project files.  I couldn't get the 32bit and 64 bit to work correctly.  
 
 cd..
 
-@echo Over changes to cjose and mod_auth_openidc so they compile on windows
+:VCPKG_COMPLETE
+
+@ECHO Over changes to cjose and mod_auth_openidc so they compile on windows
 xcopy changes\*.* /r /q /y /s
 
-@echo Downloading Apache http x64 zip files.
+@ECHO Downloading Apache http x64 zip files.
+
+if exist ".\target\httpd-2.4.51-win64-VS16" goto APACHE_DOWNLOADED
+
 powershell .\download.ps1
+
+:APACHE_DOWNLOADED
 
 call build.cmd
 
-@echo Start Time %STARTTIME%
-@echo Stop Time %time%
+@ECHO Start Time %STARTTIME%
+@ECHO Stop Time %time%
