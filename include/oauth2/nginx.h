8f21720a630cab4d5c23132d2450ae1290afe918
Hans Zandbelt <hans.zandbelt@openidc.com>
2025-01-02 16:15:25 +0100

    update copyright year to 2025
    Signed-off-by: Hans Zandbelt <hans.zandbelt@openidc.com>

--- include/oauth2/nginx.h
+++ include/oauth2/nginx.h
@@ -3,7 +3,7 @@
 
 /***************************************************************************
  *
- * Copyright (C) 2018-2024 - ZmartZone Holding BV
+ * Copyright (C) 2018-2025 - ZmartZone Holding BV
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
